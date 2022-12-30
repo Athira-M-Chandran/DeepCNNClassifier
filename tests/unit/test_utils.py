922930dbc6143a2901160ed1d69d542b10070031
Athira-M-Chandran <athiramchandran90@gmail.com>
2022-12-30 23:39:20 +0530

    test

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -4,20 +4,22 @@ from pathlib import Path
 from box import ConfigBox
 from ensure.main import EnsureError
 
-yaml_file = [
-    "tests/data/empty.yaml",
-    "tests/data/demo.yaml"
-]
+class Test_read_yaml:
 
-def test_read_yaml_empty():
-    with pytest.raises(ValueError):
-        read_yaml(Path(yaml_file[0]))
-        
-def test_read_yaml_return_type():
-    response = read_yaml(Path(yaml_file[-1]))   
-    assert isinstance(response,ConfigBox)
+    yaml_file = [
+        "tests/data/empty.yaml",
+        "tests/data/demo.yaml"
+    ]
 
-@pytest.mark.parametrize("path_to_yaml",yaml_files)
-def test_read_yaml_bad_type():
-    with pytest.raises(EnsureError):
-        read_yaml(Path(path_yaml_file))
+    def test_read_yaml_empty(self):
+        with pytest.raises(ValueError):
+            read_yaml(Path(self.yaml_file[0]))
+            
+    def test_read_yaml_return_type(self):
+        response = read_yaml(Path(self.yaml_file[-1]))   
+        assert isinstance(response,ConfigBox)
+
+    @pytest.mark.parametrize("path_to_yaml", yaml_file)
+    def test_read_yaml_bad_type(self,path_to_yaml):
+        with pytest.raises(EnsureError):
+            read_yaml(path_to_yaml)
